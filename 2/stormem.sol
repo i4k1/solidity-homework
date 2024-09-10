44dc06593a9abaf28406ecccd7366799b1973750
i4k1 <99640421+i4k1@users.noreply.github.com>
2024-09-10 22:02:12 +0400

    Update stormem.sol

--- 2/stormem.sol
+++ 2/stormem.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.26;
 contract stormem {
     uint public numberPlus;
 
-    function testMemory(uint input) public pure returns (uint) {
-        uint result = input * 2;
-        return result;
+    function testMemory(uint _input) public pure returns(uint) {
+        return input * 2;
     }
 
-    function testStorage(uint input) public returns (uint) {
+    function testStorage(uint _input) public returns(uint) {
         numberPlus += input;
         return numberPlus;
     }
